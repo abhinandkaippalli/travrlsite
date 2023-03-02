fa9e7f73841d9597c93ec988e9cd961be1eac6c4
Abhinand <abhinandkofficial01@gmail.com>
2023-03-02 13:03:56 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 // import './App.css';
 import React from "react";
-import PopularFlightsNearYou from "./components/pages/popularFlightsNearYou/PopularFlightsNearYou";
+// import PopularFlightsNearYou from "./components/pages/popularFlightsNearYou/PopularFlightsNearYou";
 // import PackageDetails from "./components/pages/packageDetails/PackageDetails";
 // import BookingSubmission from "./components/pages/bookNowOne/BookingSubmission";
 //import BookNowStatus from "./components/pages/bookNowStatus/BookNowStatus";
 // import Navbar from "./components/navbar/Navbar";
-// import MidBar from "./components/midbar/MidBar";
-// import HolidaySection from "./components/holiday-section/HolidaySection";
-// import VideoBanner from "./components/videoBanner/VideoBanner";
-// import ChoosePopularFlight from "./components/choosePopularFlight/ChoosePopularFlight";
-// import FlightBookingOffer from "./components/flightBookingOffer/FlightBookingOffer";
-// import TravelReview from "./components/travelReview/TravelReview";
-// import Footer from "./components/footer/Footer";
+import MidBar from "./components/midbar/MidBar";
+import HolidaySection from "./components/holiday-section/HolidaySection";
+import VideoBanner from "./components/videoBanner/VideoBanner";
+import ChoosePopularFlight from "./components/choosePopularFlight/ChoosePopularFlight";
+import FlightBookingOffer from "./components/flightBookingOffer/FlightBookingOffer";
+import TravelReview from "./components/travelReview/TravelReview";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 import ScrollTop from "./components/scrollTop/ScrollTop";
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
     <div className="App">
       {/* <PackageDetails /> */}
       {/* <BookingSubmission /> */}
-      <PopularFlightsNearYou />
+      {/* <PopularFlightsNearYou /> */}
       {/* <BookNowStatus /> */}
       {/* <Navbar />
       <MidBar />
@@ -30,6 +31,14 @@ function App() {
       <FlightBookingOffer />
       <TravelReview /> */}
       {/* <Footer /> */}
+      <Header />
+      <MidBar />
+      <HolidaySection />
+      <VideoBanner />
+      <ChoosePopularFlight />
+      <FlightBookingOffer />
+      <TravelReview />
+      <Footer />
       <ScrollTop />
     </div>
   );
