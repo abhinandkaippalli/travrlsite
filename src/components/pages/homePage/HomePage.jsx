0a9822cfb7ee249c1b0a4d2c6df1f2b4701dff43
Abhinand <abhinandkofficial01@gmail.com>
2023-03-02 15:24:56 +0530

    Router

--- src/components/pages/homePage/HomePage.jsx
+++ src/components/pages/homePage/HomePage.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from '../../header/Header'
+import Midbar from '../../midbar/MidBar'
+import VideoBanner from '../../videoBanner/VideoBanner'
+import HolidaySection from '../../holiday-section/HolidaySection'
+import ChoosePopularFlight from '../../choosePopularFlight/ChoosePopularFlight'
+import FlightBookingOffer from '../../flightBookingOffer/FlightBookingOffer'
+import TravelReview from '../../travelReview/TravelReview'
+import Footer from '../../footer/Footer'
+
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+        <Header />
+        <Midbar />
+        <HolidaySection />
+        <VideoBanner />
+        <ChoosePopularFlight />
+        <FlightBookingOffer />
+        <TravelReview />
+        <Footer />
+    </div>
   )
 }
 
